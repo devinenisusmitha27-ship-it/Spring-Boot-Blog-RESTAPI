003341088f65ca0bdef35774dd951c7d208c9c59
devinenisusmitha27-ship-it <devinenisusmitha27@gmail.com>
2025-11-21 22:27:34 +0530

    Create PostRepository interface for Post entity

---
+++ PostRepository.java
@@ -0,0 +1,9 @@
+package com.example.blog.repository;
+
+import com.example.blog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByAuthorId(Long authorId);
+}
